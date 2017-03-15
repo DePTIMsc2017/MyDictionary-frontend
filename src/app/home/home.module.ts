1e46fd5dba4f866462bc2f609ae6c8d6860eb01d
firstvan <firstvan@live.com>
2017-03-15 22:24:28 +0100

    Create skeleton to About page.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { routing } from '../app.routing';
 import { SharedModule } from '../shared/shared.module';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
     imports: [
@@ -10,7 +11,10 @@ import { SharedModule } from '../shared/shared.module';
       routing
     ],
     exports: [],
-    declarations: [HomeComponent],
+    declarations: [
+      HomeComponent,
+      AboutComponent
+    ],
     providers: [],
 })
 export class HomeModule { }
