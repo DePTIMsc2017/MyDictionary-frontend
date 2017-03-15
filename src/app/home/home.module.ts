baa3ce6fe36888e457a5b91dbcd53d2d253dc64c
firstvan <firstvan@live.com>
2017-03-15 22:11:10 +0100

    Added TODO comment to home page.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './home.component';
 import { routing } from '../app.routing';
-import { TranslateModule } from 'ng2-translate';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
