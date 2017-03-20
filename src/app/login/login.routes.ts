5b99182d70c00710c9aab79bd67406fc9d71d19a
Orbán István <firstvan@live.com>
2017-03-20 15:01:44 +0100

    Added Register skeleton.

--- src/app/login/login.routes.ts
+++ src/app/login/login.routes.ts
@@ -1,10 +1,15 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login-component/login.component';
+import { RegistrationComponent } from './registration-component/registration.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent
   }
 ];
 
