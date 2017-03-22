35fe3566febc49fde0a46632bd10934832f7dfdd
geczogergo <geczogergo@gmail.com>
2017-03-22 12:57:54 +0100

    regisztráció/1

--- src/app/login/registration-component/registration.component.ts
+++ src/app/login/registration-component/registration.component.ts
@@ -13,4 +13,36 @@ export class RegistrationComponent implements OnInit {
     register() {
       console.log('Register');
     }
+
+    confirm(pswd:any, nickName:any){
+      console.log(pswd);
+      if (pswd===""){
+        alert("A jelszó mező üres");
+        return;}
+      if(pswd.length<5){
+        alert("A jelszó legalább 5 karakter hosszúnak kell lennie");
+        return;}
+
+       if(!/^\w*$/.test(pswd)){
+        alert("A jelszó csak számokat, kis- és nagy betűket tartalmazhat")
+         return;
+       }
+      if(pswd===nickName){
+        alert("A felhasználónév és a jelszó nem lehet ugyan az");
+        return;
+      }
+      if(!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/.test(pswd) ){
+        alert("A jelszónak tartalmaznia kell számot, kis- és nagy betűt");
+        return;
+      }
+
+
+    }
+  pswequal(ps1:any, ps2:any){
+      if(ps1!==ps2){
+        alert("A két jelsző nem egyezik meg");
+        return;
+      }
+
+  }
 }
