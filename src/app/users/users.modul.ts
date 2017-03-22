b5dede094af2573413ccc0c0bd58279c85155757
firstvan <firstvan@live.com>
2017-03-22 19:33:17 +0100

    Added skeleton for user-profile.

---
+++ src/app/users/users.modul.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { UserProfileComponent } from './profile/user-profile.component';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [UserProfileComponent],
+    providers: [],
+})
+export class UserProfileModule { }
