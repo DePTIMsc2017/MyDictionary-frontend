1ba645328e6e0a02133a653d43b0b8a369024af5
firstvan <firstvan@live.com>
2017-04-08 12:23:27 +0200

    moved i18n files to assets dir

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -2,5 +2,5 @@ import { Http } from '@angular/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function createTranslateLoader(http: Http): TranslateHttpLoader {
-  return new TranslateHttpLoader(http, 'src/public/i18n/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
