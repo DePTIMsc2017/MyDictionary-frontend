7d93f6b81468f10c91420f10174e18410ef5520b
Péter Burai <buraipeter93@gmail.com>
2017-04-09 09:35:12 +0200

    szó hozzáadás komponens vázának létrehozása

---
+++ src/app/admin/add-word/add-word.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by Burai Péter on 2017. 04. 08..
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'add-word',
+  templateUrl: './add-word.component.html',
+  styleUrls: ['./add-word.component.css']
+})
+export class AddWordComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+
+}
