c77443bc5fb57bf5d689430a8930ffaafc0b13f8
imnotarobot <j1376813@trbvm.com>
2017-04-26 11:29:44 +0200

    Vessző

--- src/app/words/word-not-found/word-not-found.component.ts
+++ src/app/words/word-not-found/word-not-found.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
     selector: 'word-not-found',
     templateUrl: './word-not-found.component.html',
-    styleUrls: ['./word-not-found.component.css'],
+    styleUrls: ['./word-not-found.component.css']
 })
 export class WordNotFoundComponent implements OnInit {
   @Input() searched: string;
