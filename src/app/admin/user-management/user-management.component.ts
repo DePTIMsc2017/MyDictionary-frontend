4092649bb1779a8769cfc9a1b98008610c036913
geczogergo <geczogergo@gmail.com>
2017-05-02 00:15:15 +0200

    felhasznalokezelés

--- src/app/admin/user-management/user-management.component.ts
+++ src/app/admin/user-management/user-management.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Input,} from '@angular/core';
 import { usersMock } from '../../shared/mock/users.mock';
+import {LoginModel} from "../../shared/models/login.model";
 
 @Component({
   selector: 'usermanagement',
@@ -8,9 +9,38 @@ import { usersMock } from '../../shared/mock/users.mock';
 })
 export class UserManagementComponent implements OnInit {
   users=usersMock;
+  modify: boolean=true;
+  index: number;
+  selected: LoginModel[];
+  modified: LoginModel[];
 
   constructor() { }
 
   ngOnInit() { }
+
+  ngdelete(user){
+console.log(user);
+
+    this.users.splice(this.users.indexOf(user),1);
+    this.index=-1;
+
+}
+
+ngBasic(){
+
+}
+
+ngEdit(user){
+this.modify=false;
+this.index=this.users.indexOf(user);
+
+}
+
+ngSave(us){
+  console.log( );
+  this.modify=true;
+  this.index=100;
+
+}
 
 }
