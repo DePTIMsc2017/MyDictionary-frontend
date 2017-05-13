fa9248b31f946705f17b42a4af66f32ee1f09237
DESKTOP-SUQC30K\Tundi <bodistundeanna@gmail.com>
2017-05-13 20:34:19 +0200

    registration done component + Petifile

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './login-component/login.component';
 import { routing } from './login.routes';
 import { SharedModule } from '../shared/shared.module';
 import { RegistrationComponent } from './registration-component/registration.component';
+import { ReadTermsComponent } from './registration-component/read-terms.component';
 import { RegistrationDoneComponent } from './registration-component/registration-done-component/registration-done.component';
 
 @NgModule({
@@ -14,8 +15,12 @@ import { RegistrationDoneComponent } from './registration-component/registration
     declarations: [
       LoginComponent,
       RegistrationComponent,
+      ReadTermsComponent,
       RegistrationDoneComponent
     ],
     providers: [],
+    entryComponents: [
+         ReadTermsComponent
+    ]
 })
 export class LoginModule { }
